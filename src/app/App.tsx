3fc913f8caa07f3c96073290570a21d75fde66b3
Enzo <e.pecantet@edu.pucrs.br>
2025-11-04 17:44:39 -0300

    feat: nova estrutura

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,4 @@
-import { Hero } from "@/components/Hero";
-import { Navbar } from "@/components/Navbar";
-import About from "./routes/about";
-import { Projects } from "@/components/Projects";
-import { Footer } from "@/components/Footer";
-
-
 export default function App() {
-  return (
-    <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Footer />
-    </div>
-  );
+  
+  return <div>Meu App Vetryn</div>;
 }
