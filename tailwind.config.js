3165fdb1c2442475232eb6551e27ff975fe65c87
Enzo <e.pecantet@edu.pucrs.br>
2025-11-05 13:27:37 -0300

    feat: update link colors to use theme variables for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
+import theme from "./src/styles/theme.js";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#12462d",
-          content: "#7addad",
-          light: "#1c6f47",
-          dark: "#081d13",
-        },
-      },
+      colors: theme.colors,
     },
   },
   plugins: [],
